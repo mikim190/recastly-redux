fbc85f0143bf5697333abb2b6c0280471351837d
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-07-22 16:03:41 -0700

    Refactor VideoListContainer and VideoListEntry components

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,26 +1,18 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
-import changeVideo from '../actions/currentVideo.js'
 
-// var VideoPlayerContainer = () => {
-
-
-
-//   // const mapDispatchToProps = (dispatch) => {
-//   //   return {
-//   //     getVideo: (video) => dispatch(changeVideo(video)) //{type:'change_video', video: {}}
-//   //   }
-//   // }
-
-// };
-
-const mapStateToProps = (state) => {
-  // const newVideo = state.video;
-  return VideoPlayer({ newVideo });
-};
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
+var mapStateToProps = (state) => {
+  return {
+    video: state.currentVideo
+  };
+};
 
-export default connect(mapStateToProps)(VideoPlayer)
+var VideoPlayerContainer = connect(
+  mapStateToProps
+)(VideoPlayer);
 
+
+export default VideoPlayerContainer;

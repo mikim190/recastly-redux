ca1903e845d3a85c95da3a72ea280302d0f4eb28
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-07-22 16:03:06 -0700

    Refactor VideoListContainer and VideoListEntry components

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,27 +2,26 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideoList from './../actions/currentVideo.js';
 
-// var handleVideoListEntryTitleClick = (video) => {
-//   VideoListContainer();
-// }
-
-// var VideoListContainer = () => {
-
-// }
-
-const mapStateToProps = (state) => {
-  const newVideoList = state.videos;
-  return VideoList({ newVideoList, handleVideoListEntryTitleClick });
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    changeVideo: (video) => dispatch(changeVideo(video))
-  }
-}
-
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
+var mapStateToProps = (state) => {
+  return {
+    videos: state.videoList
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(VideoList)
+var mapDispatchToProps = dispatch => {
+  return {
+    handleVideoListEntryTitleClick: video => {
+      console.log(video);
+      dispatch(changeVideo(video));
+    }
+  };
+};
 
+var VideoListContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VideoList);
+
+export default VideoListContainer;

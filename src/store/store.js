69c0e2cb884ae512aa87a84382fe2d2455ca1542
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-07-22 16:08:57 -0700

    Refactor components to React Redux

--- src/store/store.js
+++ src/store/store.js
@@ -2,24 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
-//import { constants } from 'perf_hooks';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-
-const initialState = {
-  videos: [],
-  video: null
-}
-
-const configureStore = (rootReducer) => {
-  console.log(initialState);
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk)
-  )
-}
-
-const store = configureStore(rootReducer)
+var store = createStore(rootReducer, {currentVideo: exampleVideoData[0], videoList: exampleVideoData}, applyMiddleware(thunk));
 
 export default store;

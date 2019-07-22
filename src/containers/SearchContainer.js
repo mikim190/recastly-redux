77e22bcfca2985b22421c048865d06951b312395
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-07-22 16:02:21 -0700

    Refactor VideoListContainer and VideoListEntry components

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,31 +1,33 @@
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleVideoSearch from '../actions/search.js';
-import VideoListContainer from './VideoListContainer.js';
-import VideoPlayerContainer from './VideoPlayerContainer.js';
-
-// var SearchContainer = () => {
-
-//   // do these two callbacks need to return?
-
-
-// };
-
-const mapStateToProps = (state) => {
-  VideoListContainer.mapStateToProps(state);
-  VideoPlayerContainer.mapStateToProps(state);
-}
-
-const mapDispatchToProps = (dispatch) => {
-  //const currentSearch = state.value;
-  return {
-    getVideoList: (currentSearch) => dispatch(handleVideoSearch(currentSearch))
-  }
-}
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search)
+var mapStateToProps = (state) => {
+  return {
+    currentVideo: state.currentVideo,
+    videoList: state.videoList
+  };
+};
+
+
+var mapDispatchToProps = dispatch => {
+  return {
+    handleSearchInputChange: q => {
+      handleVideoSearch(q)(dispatch);
+    }
+  };
+};
+
+
+var SearchContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
+
+
+export default SearchContainer;

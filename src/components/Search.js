ffb0e1a09e65c69a4cf1cbfc7227286df1caea8c
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-07-22 15:56:00 -0700

    Refactor components to React Redux

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,26 +5,31 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      value: ''
-    };
+//     this.state = {
+//       value: ''
+//     };
   }
 
-  handleInputChange(e) {
-    this.props.getYouTubeVideos(e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  }
-
+//   handleSearchInputChange(e) { //originally handleSearchChange
+//     this.props.getYouTubeVideos(e.target.value);
+//     this.setState({
+//       value: e.target.value
+//     });
+//   }
+ onInputChange(e) {
+  this.props.handleSearchInputChange(e.target.value);
+ }
+ // once a change event occurs, it will invoke the onInputChange function and pass the event
+  // down to the handleSearchInputChange function (located in the Search Container)
+  // after the handleSearchInputChange function is invoked it will dispatch the action creator
+ 
   render() {
     return (
       <div className="search-bar form-inline">
         <input
           className="form-control"
           type="text"
-          value={this.state.value}
-          onChange={this.handleInputChange.bind(this)}
+          onChange={this.onInputChange.bind(this)}
         />
         <button className="btn hidden-sm-down">
           <span className="glyphicon glyphicon-search"></span>
